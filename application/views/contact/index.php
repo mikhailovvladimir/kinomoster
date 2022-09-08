e5269d233bfa5f9def948b2144bb6170656aea17
Vladimir <1602211@gmail.com>
2022-09-09 01:58:47 +0600

    rating page

---
+++ application/views/contact/index.php
@@ -0,0 +1,37 @@
+<div class="col-lg-push-9"> <!-- Меняем блоки местами col-lg-push-3 -->
+
+    <form role="search" class="visible-xs">
+        <div class="form-group">
+            <div class="input-group">
+                <input type="search" class="form-control input-lg" placeholder="Ваш запрос">
+                <div class="input-group-btn">
+                    <button class="btn btn-default btn-lg" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                </div>
+            </div>
+        </div>
+    </form>
+
+    <h1>Контакты</h1>
+    <hr>
+
+    <p>Отправьте ваш отзыв о портале КиноМонстр</p>
+
+
+
+    <form>
+        <div class="form-group">
+            <input type="text" placeholder="ваше имя" class="form-control input-lg">
+        </div>
+        <div class="form-group">
+            <input type="email" placeholder="ваш e-mail" class="form-control input-lg">
+        </div>
+        <div class="form-group">
+            <textarea class="form-control"></textarea>
+        </div>
+        <button class="btn btn-lg btn-warning pull-right">отправить</button>
+    </form>
+
+    <div class="margin-8 clear"></div>
+
+
+</div>
